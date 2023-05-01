af26cadb6448b3e9957660851183cb3aec97fee9
Lucas <lucaswoodyard@yahoo.com>
2023-04-30 22:09:28 -0500

    commit tree 9131dbd26bca731a2d14467fdfabe9dfe2b2f919

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, color, sex) VALUES ('Mr. Choclolate', 4, 'brown', 'M');
+
+INSERT INTO bears (name, age, color, sex) VALUES ('Rowdy', 6, 'black', 'M');
+
+INSERT INTO bears (name, age, color, sex) VALUES ('Tabitha', 8, 'light brown', 'F');
+
+INSERT INTO bears (name, age, color, sex) VALUES ('Sergeant Brown', 3, 'dark brown', 'M');
+
+INSERT INTO bears (name, age, color, sex) VALUES ('Melissa', 12, 'black', 'F');
+
+INSERT INTO bears (name, age, color, sex) VALUES ('Grinch', 4, 'black', 'M');
+
+INSERT INTO bears (name, age, color, sex) VALUES ('Wendy', 9, 'blond', 'F');
+
+INSERT INTO bears (name, age, color, sex) VALUES (NULL, NULL, 'black', 'F');
